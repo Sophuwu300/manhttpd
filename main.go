c9bc6b6dc90b108c04e640ce8aba97c9958ba6b5
sophuwu <sophie@skisiel.com>
2023-12-29 21:28:07 +0100

    style

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"bytes"
-	"log"
+	"fmt"
 	"net/http"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
@@ -11,15 +12,22 @@ import (
 
 func main() {
 	http.HandleFunc("/cgi-bin/man/", handleWIS)
-	log.Println("Starting server on http://localhost:1234")
-	log.Fatal(http.ListenAndServe(":1234", nil))
+	http.HandleFunc("/style.css", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/css; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		b, _ := os.ReadFile("style.css")
+		w.Write(b)
+		b, _ = os.ReadFile("font.css")
+		w.Write(b)
+	})
+	http.Handle("/", http.RedirectHandler("/cgi-bin/man/man2html", http.StatusTemporaryRedirect))
+	http.ListenAndServe("0.0.0.0:1234", nil)
 }
 
 func handleWIS(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
 	exe := filepath.Base(r.URL.Path)
 	if !(exe == "man2html" || exe == "mansearch" || exe == "mansec" || exe == "manwhatis") {
-		http.Error(w, "Not found", http.StatusNotFound)
+		http.Redirect(w, r, "/cgi-bin/man/man2html", http.StatusTemporaryRedirect)
 		return
 	}
 	q := ""
@@ -29,12 +37,17 @@ func handleWIS(w http.ResponseWriter, r *http.Request) {
 	cmd := exec.Command("/usr/lib/cgi-bin/man/" + exe)
 	var buff bytes.Buffer
 	cmd.Env = append(cmd.Env, "QUERY_STRING="+q, "REQUEST_METHOD="+r.Method, "SERVER_NAME=localhost:1234")
-	// cmd.Env = append(cmd.Env, "MANPATH=/usr/man:/usr/share/man:/usr/local/man:/usr/local/share/man:/usr/X11R6/man:/opt/man:/snap/man")
+	cmd.Env = append(cmd.Env, "MANPATH=/usr/man:/usr/share/man:/usr/local/man:/usr/local/share/man:/usr/X11R6/man:/opt/man:/snap/man")
 	cmd.Stdout = &buff
 	err := cmd.Run()
 	if err != nil {
 		http.Redirect(w, r, "/cgi-bin/man/man2html", http.StatusTemporaryRedirect)
 		return
 	}
-	buff.WriteTo(w)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	page := buff.String()
+	page = page[strings.Index(page, "<!"):]
+	i := strings.Index(page, "</HEAD>")
+	fmt.Fprint(w, page[:i], `<link rel="stylesheet" href="/style.css">`, page[i:])
 }
