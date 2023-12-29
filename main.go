62c48f123a7de0f4e8f2ae1eda22fdfc44b8098d
sophuwu <sophie@skisiel.com>
2023-12-29 22:16:55 +0100

    awesome shit

--- main.go
+++ main.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
 	"fmt"
 	"net/http"
-	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 )
 
+//go:embed font.css
+var font []byte
+
+//go:embed dark_theme.css
+var css []byte
+
+func init() {
+	css = append(css, font...)
+}
+
 func main() {
 	http.HandleFunc("/cgi-bin/man/", handleWIS)
 	http.HandleFunc("/style.css", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/css; charset=utf-8")
+		w.Header().Set("Content-Length", fmt.Sprint(len(css)))
 		w.WriteHeader(http.StatusOK)
-		b, _ := os.ReadFile("style.css")
-		w.Write(b)
-		b, _ = os.ReadFile("font.css")
-		w.Write(b)
+		w.Write(css)
 	})
 	http.Handle("/", http.RedirectHandler("/cgi-bin/man/man2html", http.StatusTemporaryRedirect))
 	http.ListenAndServe("0.0.0.0:1234", nil)
@@ -37,7 +45,7 @@ func handleWIS(w http.ResponseWriter, r *http.Request) {
 	cmd := exec.Command("/usr/lib/cgi-bin/man/" + exe)
 	var buff bytes.Buffer
 	cmd.Env = append(cmd.Env, "QUERY_STRING="+q, "REQUEST_METHOD="+r.Method, "SERVER_NAME=localhost:1234")
-	cmd.Env = append(cmd.Env, "MANPATH=/usr/man:/usr/share/man:/usr/local/man:/usr/local/share/man:/usr/X11R6/man:/opt/man:/snap/man")
+	// cmd.Env = append(cmd.Env, "MANPATH=/usr/man:/usr/share/man:/usr/local/man:/usr/local/share/man:/usr/X11R6/man:/opt/man:/snap/man")
 	cmd.Stdout = &buff
 	err := cmd.Run()
 	if err != nil {
@@ -49,5 +57,10 @@ func handleWIS(w http.ResponseWriter, r *http.Request) {
 	page := buff.String()
 	page = page[strings.Index(page, "<!"):]
 	i := strings.Index(page, "</HEAD>")
-	fmt.Fprint(w, page[:i], `<link rel="stylesheet" href="/style.css">`, page[i:])
+	if i == -1 {
+		i = strings.Index(page, "</head>")
+	}
+	fmt.Fprintln(w, page[:i])
+	fmt.Fprintln(w, `<link rel="stylesheet" href="/style.css">`)
+	fmt.Fprintln(w, page[i:])
 }
